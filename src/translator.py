d2bd3c56ca15d2f10e3892a5cc024bdf440c617e
Starstreak <Starstreak1@proton.me>
2025-06-16 13:52:20 -0700

    - Started translator.py and created the test file.
    - Wrote test for testing write_push_pop, which for now only pushes,

--- src/translator.py
+++ src/translator.py
@@ -2,6 +2,11 @@
 src/translator.py
 Converts VM code instructions to machine / assembly code.
 """
+from src.data_storage import CommandType
+
+command_map: dict = {
+    "push": ["@SP", "AM=M+1", "A=A-1", "M=D"]
+}
 
 class Translator:
     """
@@ -9,3 +14,8 @@ class Translator:
     """
     def __init__(self):
         pass
+
+    def write_push_pop(self, command: str):
+        """
+        Takes a command and depending on whether it's C_PUSH or C_POP, operate on it.
+        """
--- test/translator_test.py
+++ test/translator_test.py
@@ -21,3 +21,12 @@ def test_object_creation(setup_resources):
     """
     translator = setup_resources["translator"]
     assert translator is not None
+
+
+def test_write_push_pop(setup_resources):
+    """
+    Test that the push function properly translates.
+    """
+    translator = setup_resources["translator"]
+    translated_push_value: list[str] = translator.write_push_pop()
+    assert translated_push_value == ["@SP", "AM=M+1", "A=A-1", "M=D"]
